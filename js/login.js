5d5d45b6096435adc38ffc30ea7e9b93c72ccffe
raiansakil <aburaian182@gmail.com>
2022-02-10 12:13:53 +0600

    added

---
+++ js/login.js
@@ -0,0 +1,10 @@
+document.getElementById("login-submit").addEventListener("click", function () {
+    const inputEmail = document.getElementById("user-email");
+    const userEmail = inputEmail.value;
+    const inputPassword = document.getElementById("user-password");
+    const userPassword = inputPassword.value;
+    if (userEmail == "sontankebap@gmail.com" && userPassword == "bapkebata007") {
+        window.location.href = "banking.html";
+    }
+
+});
